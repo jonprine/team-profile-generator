e071447f3280109827b40136346288e8445411e8
Vox Humana <jonprine@gmail.com>
2020-08-07 11:44:22 -0500

    added initial prompt. fixed inquirer typo

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const inquirer = requirer('inquirer');
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// array of questions for user
+function promptManager() {
+    inquirer
+        .prompt([
+    {
+        type: 'input',
+        name: 'managerName',
+        message: 'What is the team manager\'s name?'
+        
+    },
+    {
+        type: 'input',
+        name: 'managerId',
+        message: 'What is the team manager\'s id?'
+    },
+    {
+        type: 'input',
+        name: 'managerEmail',
+        message: 'What is the team manager\'s email?'
+    },
+    {
+        type: 'input',
+        name: 'managerNumber',
+        message: 'What is the team manager\'s office number?'
+    },
+
+])
+.then(answers => {
+    console.log(answers);
+    const addManager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerNumber);
+    employees.push(addManager);
+    
+})
+.catch(error => {
+    console.log('Unable to add team member. Please try again.')
+})
+
+};
