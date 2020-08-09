b655b7e5991260b27674e157aac0eb70665000a6
Vox Humana <jonprine@gmail.com>
2020-08-09 13:27:26 -0500

    separated user input selections into functions

--- index.js
+++ index.js
@@ -1,105 +1,187 @@
-const Manager = require('./lib/Manager');
-const Render = require('./src/htmlrenderer');
+const Manager = require("./lib/Manager");
+const Render = require("./src/htmlrenderer");
 
-const inquirer = require('inquirer');
-const fs = require('fs');
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+const renderEmployees = require("./src/htmlrenderer");
 
 const employees = [];
 
 function init() {
-    promptManager();
+  promptManager();
 }
 
 // array of questions for user
-function promptManager() {
-    inquirer
-        .prompt([
-    {
-        type: 'input',
-        name: 'managerName',
-        message: 'What is the team manager\'s name?'
-        
-    },
-    {
-        type: 'input',
-        name: 'managerId',
-        message: 'What is the team manager\'s ID number?'
-    },
-    {
-        type: 'input',
-        name: 'managerEmail',
-        message: 'What is the team manager\'s email?'
-    },
-    {
-        type: 'input',
-        name: 'officeNumber',
-        message: 'What is the team manager\'s office number?'
-    },
-    {
-        type: 'list',
-        name: 'teamList',
-        message: 'Which type of team member would you like to add?',
-        choices: ['Engineer', 'Intern', 'None'],
-        default: 'None'
-    },
-    {
-        type: 'input',
-        name: 'engineerName',
-        message: 'What is the engineer\'s name?',
-    },
-    {
-        type: 'input',
-        name: 'engineerId',
-        message: 'What is the engineer\'s ID number?'
-    },
-    {
-        type: 'input',
-        name: 'engineerEmail',
-        message: 'What is the engineer\'s email address?'
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'What is the engineer\'s Github username?'
-    },
-    {
-        type: 'input',
-        name: 'internName',
-        message: 'What is your intern\'s name?'
-    },
-    {
-        type: 'input',
-        name: 'internId',
-        message: 'What is your intern\'s ID?'
-    },
-    {
-        type: 'input',
-        name: 'internEmail',
-        message: 'What is your intern\'s email?'
-    },
-    {
-        type: 'input',
-        name: 'school',
-        message: 'What is your intern\'s school?'
-    }
 
-])
-.then(answers => {
-    const addManager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.officeNumber);
-    employees.push(addManager);
-    console.log(addManager);
-    Render(employees);
-    
-})
-.catch(error => {
-    if(error.isTtyError) {
-        console.log('Team member could not be added. Please try again');
+// questons for team manager
+function promptManager() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "managerName",
+        message: "What is the team manager's name?",
+      },
+      {
+        type: "input",
+        name: "managerId",
+        message: "What is the team manager's ID number?",
+      },
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "What is the team manager's email?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the team manager's office number?",
+      },
+    ])
+    .then((answers) => {
+      const addManager = new Manager(
+        answers.managerName,
+        answers.managerId,
+        answers.managerEmail,
+        answers.officeNumber
+      );
+      employees.push(addManager);
+      console.log(addManager);
+      Render(employees);
+      addNewMember();
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        console.log("Team member could not be added. Please try again");
       } else {
         // Something else when wrong
       }
     });
-};
+}
 
+// function to add another team member per selection from list
+
+function addNewMember() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "teamList",
+        message: "Which type of team member would you like to add?",
+        choices: ["Engineer", "Intern", "None"],
+        default: "None",
+      },
+    ])
+    .then((answers) => {
+      if (answers.teamList === "Engineer") {
+        addEngineer();
+      } else if (answers.teamList === "Intern") {
+        addIntern();
+      } else if (answers.teamList === "None") {
+        renderEmployees();
+      }
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        console.log("Team member could not be added. Please try again");
+      } else {
+        // Something else when wrong
+      }
+    });
+}
+
+// if engineer is selected
+function addEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "engineerName",
+        message: "What is the engineer's name?",
+      },
+      {
+        type: "input",
+        name: "engineerId",
+        message: "What is the engineer's ID number?",
+      },
+      {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is the engineer's email address?",
+      },
+      {
+        type: "input",
+        name: "engineerGithub",
+        message: "What is the engineer's Github username?",
+      },
+    ])
+    .then((answers) => {
+      const addEngineer = new Engineer(
+        answers.engineerName,
+        answers.engineerId,
+        answers.engineerEmail,
+        answers.engineerGithub
+      );
+      employees.push(addEngineer);
+      console.log(addEngineer);
+      Render(employees);
+      addNewMember();
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        console.log("Team member could not be added. Please try again");
+      } else {
+        // Something else when wrong
+      }
+    });
+}
+
+// if intern is selected
+function addIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "internName",
+        message: "What is your intern's name?",
+      },
+      {
+        type: "input",
+        name: "internId",
+        message: "What is your intern's ID?",
+      },
+      {
+        type: "input",
+        name: "internEmail",
+        message: "What is your intern's email?",
+      },
+      {
+        type: "input",
+        name: "internSchool",
+        message: "What is your intern's school?",
+      },
+    ])
+    .then((answers) => {
+      const addIntern = new Intern(
+        answers.internName,
+        answers.internId,
+        answers.internEmail,
+        answers.internSchool
+      );
+      employees.push(addIntern);
+      console.log(addIntern);
+      Render(employees);
+      addNewMember();
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        console.log("Team member could not be added. Please try again");
+      } else {
+        // Something else when wrong
+      }
+    });
+}
 
 // function call to initialize program
 init();
